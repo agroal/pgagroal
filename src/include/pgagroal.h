21422ea08d012e5f7e74851c0afc747b986d2fdb
jesperpedersen <jesper.pedersen@redhat.com>
2020-08-25 07:55:40 -0400

    Next is 0.8.2

--- src/include/pgagroal.h
+++ src/include/pgagroal.h
@@ -41,7 +41,7 @@ extern "C" {
 #include <sys/types.h>
 #include <openssl/ssl.h>
 
-#define VERSION "0.8.1"
+#define VERSION "0.8.2"
 
 #define PGAGROAL_HOMEPAGE "https://agroal.github.io/pgagroal/"
 #define PGAGROAL_ISSUES "https://github.com/agroal/pgagroal/issues"
