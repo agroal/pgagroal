339cb9a31500cf3210255952d77596e4f891f0fb
jesperpedersen <jesper.pedersen@redhat.com>
2021-11-09 08:08:50 -0500

    Next is 1.3.4

--- src/include/pgagroal.h
+++ src/include/pgagroal.h
@@ -41,7 +41,7 @@ extern "C" {
 #include <sys/types.h>
 #include <openssl/ssl.h>
 
-#define VERSION "1.3.3"
+#define VERSION "1.3.4"
 
 #define PGAGROAL_HOMEPAGE "https://agroal.github.io/pgagroal/"
 #define PGAGROAL_ISSUES "https://github.com/agroal/pgagroal/issues"
