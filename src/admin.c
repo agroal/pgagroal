agroal/pgagroal

commit 3b43af89b9c55a9b8247ed62022481390e720ba1

admin: support password through environment
Passing the password through the "--password" command line argument is
potentially insecure, as the value would be exposed in a process listing
or shell history. Read an environment variable as an alternative.

Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

src/admin.c
@@ -387,28 +387,37 @@ master_key(char* password, bool generate_pwd, int pwd_length, int32_t output_for
 
    if (password == NULL)
    {
-      if (!generate_pwd)
-      {
-         while (password == NULL)
-         {
-            printf("Master key (will not echo): ");
-            password = pgagroal_get_password();
-            printf("\n");
-
-            if (password != NULL && strlen(password) < MIN_PASSWORD_LENGTH)
-            {
-               printf("Invalid key length, must be at least %d chars.\n", MIN_PASSWORD_LENGTH);
-               free(password);
-               password = NULL;
-            }
-         }
-      }
-      else
+      if (generate_pwd)
       {
          if (pgagroal_generate_password(pwd_length, &password))
          {
             do_free = false;
             goto error;
+         }
+      }
+      else
+      {
+         password = secure_getenv("PGAGROAL_PASSWORD");
+
+         if (password == NULL)
+         {
+            while (password == NULL)
+            {
+               printf("Master key (will not echo): ");
+               password = pgagroal_get_password();
+               printf("\n");
+
+               if (password != NULL && strlen(password) < MIN_PASSWORD_LENGTH)
+               {
+                  printf("Invalid key length, must be at least %d chars.\n", MIN_PASSWORD_LENGTH);
+                  free(password);
+                  password = NULL;
+               }
+            }
+         }
+         else
+         {
+            do_free = false;
          }
       }
    }
@@ -586,15 +595,25 @@ password:
       }
       else
       {
-         printf("Password : ");
+         password = secure_getenv("PGAGROAL_PASSWORD");
 
-         if (password != NULL)
+         if (password == NULL)
          {
-            free(password);
-            password = NULL;
-         }
+            printf("Password : ");
 
-         password = pgagroal_get_password();
+            if (password != NULL)
+            {
+               free(password);
+               password = NULL;
+            }
+
+            password = pgagroal_get_password();
+         }
+	       else
+         {
+            do_free = false;
+            do_verify = false;
+         }
       }
       printf("\n");
    }
@@ -815,15 +834,25 @@ password:
             }
             else
             {
-               printf("Password : ");
+               password = secure_getenv("PGAGROAL_PASSWORD");
 
-               if (password != NULL)
+               if (password == NULL)
                {
-                  free(password);
-                  password = NULL;
-               }
+                  printf("Password : ");
 
-               password = pgagroal_get_password();
+                  if (password != NULL)
+                  {
+                     free(password);
+                     password = NULL;
+                  }
+
+                  password = pgagroal_get_password();
+               }
+	             else
+               {
+                  do_free = false;
+                  do_verify = false;
+               }
             }
             printf("\n");
          }

AUTHORS
@@ -15,3 +15,4 @@ Eugenio Gigante <giganteeugenio2@gmail.com>
 Haoran Zhang <andrewzhr9911@gmail.com>
 Mohanad Khaled <mohanadkhaled87@gmail.com>
 Christian Englert <code@c.roboticbrain.de>
+Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

commit 400e019aff3618db442395972c0634e3128ce58f

admin: remove stray tab characters
Accidentally introduced, remove to align with rest of the file.

Fixes: 3b43af89b9c5 ("admin: support password through environment")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

src/admin.c
@@ -609,7 +609,7 @@ password:
 
             password = pgagroal_get_password();
          }
-	       else
+         else
          {
             do_free = false;
             do_verify = false;
@@ -848,7 +848,7 @@ password:
 
                   password = pgagroal_get_password();
                }
-	             else
+               else
                {
                   do_free = false;
                   do_verify = false;